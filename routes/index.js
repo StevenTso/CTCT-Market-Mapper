5fcb876135f05ca5e5e6e1dc3130282bda2109fb
McAfee, Patrick <pmcafee@constantcontact.com>
2014-11-06 14:16:00 -0500

    hid main page customers

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,6 @@ var router = express.Router();
 var dataBridge = require("../ctctDataBridge");
 var keys = require("../public/javascripts/keys");
 
-console.log("API_KEY: "+keys.CTCT_API_KEY);
-
 /* GET users listing. */
 router.get('/', function(req, res) {
 	dataBridge.getDefaultContacts(keys.CTCT_API_KEY, keys.CTCT_TOKEN_ACCESS,
